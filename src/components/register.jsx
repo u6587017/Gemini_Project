ecb9f3cc1af4de70f227f85b14e91cbcc7e1f33e
Thanawich Juengkijthanawat <thanawich.jue@student.mahidol.edu>
2025-03-29 00:36:18 +0700

    Update register.jsx

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,11 +1,18 @@
 import background from '../assets/background.webp'
 import { Select } from '@headlessui/react'
+import { Link } from "react-router-dom"
 
- 
 function Register(){
     return(
         <div style={{background: `url(${background})`}} className="min-h-screen flex items-center justify-center p-4">
-        <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8 border-3 border-indigo-500">
+        <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8 pt-4 border-3 border-indigo-500">
+            <Link to="/"
+                className="inline-flex items-start pr-3 py-1.5 rounded-md text-indigo-500 hover:bg-indigo-50">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18">
+                    </path>
+                </svg>
+            </Link>
                     <h2 className="text-2xl font-bold text-gray-900 mb-6 text-center">Register</h2>
         
         <form className="space-y-4">
