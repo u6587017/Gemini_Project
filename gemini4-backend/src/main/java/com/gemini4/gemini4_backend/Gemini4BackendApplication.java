d5dbc3ac6a9b870bbe909d73e1ae740ea85767de
Thanawich Juengkijthanawat <thanawich.jue@student.mahidol.edu>
2025-03-29 16:06:59 +0700

    gemini-backend

---
+++ gemini4-backend/src/main/java/com/gemini4/gemini4_backend/Gemini4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.gemini4.gemini4_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gemini4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gemini4BackendApplication.class, args);
+	}
+
+}
---
+++ gemini4-backend/src/test/java/com/gemini4/gemini4_backend/Gemini4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemini4.gemini4_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gemini4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
